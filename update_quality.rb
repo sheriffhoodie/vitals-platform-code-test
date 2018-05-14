35acc49a0e1ee81eb93f9472e9f6ad891dce9d2f
Max <xamskier@aol.com>
2018-05-13 20:47:57 -0400

    finished, all tests pass

--- update_quality.rb
+++ update_quality.rb
@@ -1,49 +1,84 @@
 require 'award'
 
 def update_quality(awards)
+
   awards.each do |award|
-    if award.name != 'Blue First' && award.name != 'Blue Compare'
-      if award.quality > 0
-        if award.name != 'Blue Distinction Plus'
-          award.quality -= 1
-        end
-      end
+    if award.name == 'Blue Distinction Plus'
+      next
+    elsif award.name == 'Blue First'
+      update_Blue_First_quality(award)
+    elsif award.name == 'Blue Compare'
+      update_Blue_Compare_quality(award)
+    elsif award.name == 'Blue Star'
+      update_Blue_Star_quality(award)
     else
-      if award.quality < 50
-        award.quality += 1
-        if award.name == 'Blue Compare'
-          if award.expires_in < 11
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-          if award.expires_in < 6
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if award.name != 'Blue Distinction Plus'
-      award.expires_in -= 1
-    end
-    if award.expires_in < 0
-      if award.name != 'Blue First'
-        if award.name != 'Blue Compare'
-          if award.quality > 0
-            if award.name != 'Blue Distinction Plus'
-              award.quality -= 1
-            end
-          end
-        else
-          award.quality = award.quality - award.quality
-        end
-      else
-        if award.quality < 50
-          award.quality += 1
-        end
-      end
+      update_normal_quality(award)
     end
   end
+
+end
+
+def update_normal_quality(award)
+  if award.quality >= 1 && award.expires_in > 0
+    award.quality -= 1
+  elsif award.quality >= 2 && award.expires_in < 0
+    award.quality -= 2
+  elsif award.quality >= 2 && award.expires_in == 0
+    award.quality -= 2
+  elsif award.quality == 1 && award.expires_in < 0
+    award.quality -= 1
+  end
+  award.expires_in -= 1
+end
+
+def update_Blue_Star_quality(award)
+  if award.quality >= 2 && award.expires_in > 0
+    award.quality -= 2
+  elsif award.quality == 1 && award.expires_in > 0
+    award.quality -= 1
+  end
+  if award.quality >= 4 && award.expires_in <= 0
+    award.quality -= 4
+  end
+  award.expires_in -= 1
+end
+
+def update_Blue_First_quality(award)
+  if award.quality < 50 && award.quality + 1 <= 50
+    award.quality += 1
+  end
+  if award.expires_in <= 0 && award.quality + 1 <= 50
+    award.quality += 1
+  end
+
+  award.expires_in -= 1
+end
+
+def update_Blue_Compare_quality(award)
+
+  if award.expires_in >= 6 && award.expires_in < 11
+
+    if award.quality < 50 && award.quality + 2 <= 50
+      award.quality += 2
+    elsif award.quality < 50 && award.quality + 1 <= 50
+      award.quality += 1
+    end
+
+  elsif award.expires_in < 6 && award.expires_in > 0
+
+    if award.quality < 50 && award.quality + 3 <= 50
+      award.quality += 3
+    elsif award.quality < 50 && award.quality + 2 <= 50
+      award.quality += 2
+    elsif award.quality < 50 && award.quality + 1 <= 50
+      award.quality += 1
+    end
+
+  elsif award.expires_in <= 0
+    award.quality = 0
+  else
+    award.quality += 1
+  end
+
+  award.expires_in -= 1
 end
